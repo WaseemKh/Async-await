690cbad66566e06d7b473b31a3b9318f82fb9ed5
Waseem Khalid <waseemsabra2@gmail.com>
2022-10-23 13:23:35 +0300

    without await long process -un asynch

--- Async-await/Program.cs
+++ Async-await/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+	 public static  void  Main()
+	{
+		LongProcess();
+		ShortProcess();
+
+		Console.ReadKey();
+	}
+
+    public static async void LongProcess()
+    {
+        Console.WriteLine("Long Process Started....");
+         Thread.Sleep(3000);
+        Console.WriteLine("3 second Later");
+        Console.WriteLine("End Long Process !");
+    }
+    static void ShortProcess()
+    {
+        Console.WriteLine("Start SHORT Process ----");
+        Console.WriteLine("END SHORT Process ----");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
