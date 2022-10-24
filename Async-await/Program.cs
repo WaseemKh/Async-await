4c0cc155d3fdee5ee48b1d0828e443908bc69c73
Waseem Khalid <waseemsabra2@gmail.com>
2022-10-24 17:21:44 +0300

    Call Method after return or new line in same method

--- Async-await/Program.cs
+++ Async-await/Program.cs
@@ -6,37 +6,24 @@ public class Program
 {
     public static async Task Main()
     {
-        Task<long> num1 = LongProcess();
-
-       // Task<long> num2 = ShortProcess();
-
-        var total = await num1 + 3;
-        Console.WriteLine($"Total is :"+ total);
+        longProcess();
         Console.ReadKey();
     }
 
-    public static  async Task<long> LongProcess()
+    public static async Task data()
     {
-        List<int> list = new List<int>();
-     
-        for (var i = 0; i <= 10; i++)
-        {
+        Thread.Sleep(5000);
+        Console.WriteLine("Data Completed");
 
-            list.Add(i);
-        }
-
-        //get total for it 
-
-        long total = 0;
-        list.ForEach(i =>
-        {
-            total += i;
-        });
-        return total;
-
-        //can use this (n*n+1) /2 however i want use async and await just
     }
-   
+
+    public static async void longProcess()
+    {
+        Task.Run(() => data());
+        Console.WriteLine("Data Still Calling");
+        //return
+    }
+
 }
 
 
